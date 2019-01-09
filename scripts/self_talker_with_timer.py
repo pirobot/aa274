2f46bf462d8806f026f9ef1343c6247779b19c45
Patrick Goebel <patrick@pirobot.org>
2019-01-09 08:27:51 -0800

    Updated self_talker.py

--- scripts/self_talker_with_timer.py
+++ scripts/self_talker_with_timer.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+import time
+
+pub_time_ros = None
+pub_time = None
 
 def callback(msg):
+    delta_time = pub_time - time.time()
+    delta_time_ros = pub_time_ros - rospy.get_time()    
+
     rospy.loginfo("I heard: %s", msg.data)
 
+    rospy.logwarn("delta time ROS: %.5f, delta time: %.5f, delta-delta time: %.3f (microsec)", delta_time_ros, delta_time, (delta_time_ros - delta_time) * 1000000)
+
 def self_talker():
+    global pub_time, pub_time_ros
+
     rospy.init_node('talker', anonymous=True)
 
     sub = rospy.Subscriber("chatter", String, callback)    
@@ -19,11 +30,11 @@ def self_talker():
 
     while not rospy.is_shutdown():
         msg = "Greetings humans!"
+
+        pub_time = time.time()
+        pub_time_ros = rospy.get_time()
         
         pub.publish(msg)
-
-        rospy.logwarn("Publishing %s", msg)
-        
         ros_rate.sleep()
 
 if __name__ == '__main__':
